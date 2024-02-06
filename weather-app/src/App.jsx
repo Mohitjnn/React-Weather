20bc3027c6039c140804c4c7827befdfc431880d
Mohitjnn <jnmohit29@gmail.com>
2024-02-07 01:36:22 +0530

    UI prepared,static

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,13 +3,44 @@ import Icons from "./components/Icons";
 import "./App.css";
 import Temp from "./components/Temp";
 import Extra from "./components/Extra";
-
+import FutureData from "./components/FutureData";
 function App() {
+  let FutureWeatherData = [
+    {
+      weather: "sun-shower",
+      mintemp: "-20°",
+      maxtemp: "20°",
+      day: "Tom",
+    },
+    {
+      weather: "thunder-storm",
+      mintemp: "-10°",
+      maxtemp: "25°",
+      day: "Fri",
+    },
+    {
+      weather: "flurries",
+      mintemp: "-14°",
+      maxtemp: "33°",
+      day: "Sat",
+    },
+    {
+      weather: "sunny",
+      mintemp: "-30°",
+      maxtemp: "46°",
+      day: "Sun",
+    },
+  ];
   return (
-    <center className="h-screen flex items-center justify-center flex-col">
+    <center className="h-screen flex items-center justify-center flex-col ">
       <Header />
       <section className="w-full flex flex-row items-center justify-evenly my-5">
         <Icons weather={"rainy"} /> <Temp></Temp> <Extra></Extra>
+      </section>
+      <section className="w-full flex flex-row items-center justify-center mt-15">
+        {FutureWeatherData.map((data) => {
+          return <FutureData data={data}></FutureData>;
+        })}
       </section>
     </center>
   );
---
+++ weather-app/src/components/FutureData.jsx
@@ -0,0 +1,15 @@
+import Weather from "./Icons";
+
+const FutureData = ({ data }) => {
+  return (
+    <div className=" flex flex-col mx-2 weatherCard justify-center p-3.5">
+      <Weather weather={data.weather}></Weather>
+      <h1 className="mt-4 font font-light text-4xl">
+        <span>{data.mintemp}</span>/<span>{data.maxtemp}</span>
+      </h1>
+      <h1 className="mt-4 font font-black darkerColor">{data.day}</h1>
+    </div>
+  );
+};
+
+export default FutureData;
